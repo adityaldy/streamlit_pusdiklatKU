d4923b78ded8a9b0da08392797abd0533e7767f6
adityaldy <67299907+adityaldy@users.noreply.github.com>
2024-06-28 06:50:50 +0000

    coba yang gambar

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ def main() :
   st.metric(label="Temperature", value="70 F", delta="-1.2 F")
   st.markdown('# dataframe biasa')
   st.dataframe(house)
-  st.markdown('# Aggrid')
+  st.markdown('# dataframe dalam Aggrid')
   AgGrid(house, gridOptions=grid_options, enable_enterprise_modules=True, theme='streamlit')
   #st.table([x for x in range(1,5)])
 
@@ -86,6 +86,31 @@ def main() :
   #Input (Typing)
   num_input = st.number_input('Input Berapapun')
   st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
+
+ #sidebar 
+  sidebar_checkbox = st.sidebar.checkbox('Checkbox di Sidebar')
+  sidebar_radio_button = st.sidebar.radio('Pilih Menu',options=['A','B','C'])
+    
+  #columns :
+  col1, col2, col3 = st.columns(3)
+
+  with col1:
+      st.header("A cat")
+      st.image("https://static.streamlit.io/examples/cat.jpg")
+  #atau dengan assignment 
+  #image_col1 = col1.image("https://static.streamlit.io/examples/cat.jpg")
+
+  with col2:
+      st.header("A dog")
+      st.image("https://static.streamlit.io/examples/dog.jpg")
+
+  with col3:
+      st.header("An owl")
+      st.image("https://static.streamlit.io/examples/owl.jpg")
+    #expander 
+    #dengan with atau dengan assignment 
+expander = st.expander("Klik Untuk Detail ")
+expander.write('Anda Telah Membuka Detail')
 
 if __name__ == '__main__' : 
   main()
