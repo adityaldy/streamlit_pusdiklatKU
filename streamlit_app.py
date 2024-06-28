afa4f516d788422588ee55a8cf04dddb0674792b
adityaldy <67299907+adityaldy@users.noreply.github.com>
2024-06-28 06:57:46 +0000

    gambar kucing pake assignment

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,9 +96,9 @@ def main() :
 
   with col1:
       st.header("A cat")
-      st.image("https://static.streamlit.io/examples/cat.jpg")
+      #st.image("https://static.streamlit.io/examples/cat.jpg")
   #atau dengan assignment 
-  #image_col1 = col1.image("https://static.streamlit.io/examples/cat.jpg")
+  image_col1 = col1.image("https://static.streamlit.io/examples/cat.jpg")
 
   with col2:
       st.header("A dog")
